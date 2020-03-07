e5706d0ae694e0091ad18b0cfab5ef6702ccd03c
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-08 00:22:04 +0900

    onDragStart and onDragUpdate

--- src/index.js
+++ src/index.js
@@ -8,7 +8,24 @@ import Column from "./Column";
 const App = () => {
   const [dataSource, setDataSource] = useState(InitialData);
 
+  const onDragStart = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  const onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(dataSource.tasks).length
+      : 0;
+
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  };
+
   const onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -44,7 +61,11 @@ const App = () => {
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
       {dataSource.columnOrder.map(columnId => {
         const column = dataSource.columns[columnId];
         const tasks = column.taskIds.map(taskId => dataSource.tasks[taskId]);
