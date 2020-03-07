f0589b38c9d44e363aa42b2f038a86bf87696663
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-07 23:57:05 +0900

    onDragEnd

---
+++ src/ExampleResult.js
@@ -0,0 +1,24 @@
+const result1 = {
+  draggableId: "task-1",
+  type: "TYPE",
+  reason: "DROP",
+  source: {
+    droppableId: "column-1",
+    index: 0
+  },
+  destination: {
+    droppableId: "column-1",
+    index: 1
+  }
+};
+
+const result2 = {
+  draggableId: "task-1",
+  type: "TYPE",
+  reason: "DROP",
+  source: {
+    droppableId: "column-1",
+    index: 0
+  },
+  destination: null
+};
--- src/index.js
+++ src/index.js
@@ -8,7 +8,40 @@ import Column from "./Column";
 const App = () => {
   const [dataSource, setDataSource] = useState(InitialData);
 
-  const onDragEnd = result => {};
+  const onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = dataSource.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...dataSource,
+      columns: {
+        ...dataSource.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    setDataSource(newState);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
